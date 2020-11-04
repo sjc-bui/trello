1acacd59dc23b3a3a95fdfb02d52f7b66e954bfd
sjc-bui <bui@setjapan.co.jp>
2020-11-04 20:22:51 +0900

    add images

--- src/components/Nav/SideBar.jsx
+++ src/components/Nav/SideBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Drawer, Grow } from '@material-ui/core';
 import colors from '../../utils/color';
+import images from '../../utils/images';
 
 const useStyle = makeStyles((theme) => ({
     drawer: {
@@ -17,6 +18,7 @@ const useStyle = makeStyles((theme) => ({
         height: '90px',
         borderRadius: '9px',
         marginBottom: theme.spacing(2),
+        cursor: 'pointer',
     },
     optionContainer: {
         display: 'flex',
@@ -71,25 +73,29 @@ const SideBar = ({ openSideMenu, setOpenSideMenu, changeBackground }) => {
                                                 background: color
                                             }}
                                             onClick={() => changeBackground(color)}
-                                            >
+                                        >
                                         </div>
                                     );
                                 })}
                             </div>
-                        </Grow>
-                        :
+                        </Grow> :
                         <Grow in={true}>
                             <div className={classes.optionContainer}>
-                                <div
-                                    className={classes.box}
-                                    style={{
-                                        backgroundImage: 'url(https://512pixels.net/downloads/macos-wallpapers-thumbs/10-14-Night-Thumb.jpg)',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundSize: 'cover',
-                                    }}
-                                    onClick={() => changeBackground('https://512pixels.net/downloads/macos-wallpapers-thumbs/10-14-Night-Thumb.jpg')}
-                                    >
-                                </div>
+                                {images.map((image, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            className={classes.box}
+                                            style={{
+                                                backgroundImage: `url(${image})`,
+                                                backgroundRepeat: 'no-repeat',
+                                                backgroundSize: 'cover',
+                                            }}
+                                            onClick={() => changeBackground(image)}
+                                        >
+                                        </div>
+                                    );
+                                })}
                             </div>
                         </Grow>
                     }
