0bfc8ab7ffa1889469acf93026efe9ed06e4dac2
sjc-bui <bui@setjapan.co.jp>
2020-11-10 14:25:37 +0900

    markdown description

--- src/components/List/Card.jsx
+++ src/components/List/Card.jsx
@@ -9,6 +9,11 @@ import DialogContent from '@material-ui/core/DialogContent';
 import { useContext } from 'react';
 import storeApi from '../../utils/storeApi';
 import Divider from '@material-ui/core/Divider';
+import ReactMarkdown from 'react-markdown';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const useStyle = makeStyles((theme) => ({
     card: {
@@ -19,18 +24,35 @@ const useStyle = makeStyles((theme) => ({
         }
     },
     textArea: {
-        minWidth: '300px',
-        maxWidth: '400px',
+        minWidth: '500px',
+        maxWidth: '600px',
         marginTop: theme.spacing(1),
     },
     explainTitle: {
-        marginTop: theme.spacing(1),
+        marginTop: theme.spacing(2),
+        borderBottom: '1px solid #ddd',
+        paddingBottom: theme.spacing(2),
     },
     explainText: {
         fontSize: '14px',
     },
     subtitle: {
         fontSize: '11px',
+    },
+    editDes: {
+        textDecoration: 'underline',
+        color: '#727272',
+        '&:hover': {
+            cursor: 'pointer',
+        }
+    },
+    btn: {
+        cursor: 'pointer',
+    },
+    active: {
+        cursor: 'pointer',
+        fontWeight: 600,
+        padding: theme.spacing(0.5, 1, 0.5, 1),
     }
 }));
 
@@ -39,6 +61,7 @@ const DialogBox = ({ show, setShow, title, description, handleDeleteCard, listTi
     const [newTitle, setNewTitle] = useState(title);
     const [newDes, setNewDes] = useState(description);
     const [changed, setChanged] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const handleClose = () => {
         setShow(false);
@@ -60,6 +83,12 @@ const DialogBox = ({ show, setShow, title, description, handleDeleteCard, listTi
         handleUpdateCardTitle(newTitle, newDes);
     }
 
+    const onFocus = (e) => {
+        var temp_val = e.target.value;
+        e.target.value = '';
+        e.target.value = temp_val;
+    }
+
     return (
         <div>
             <Dialog
@@ -71,8 +100,36 @@ const DialogBox = ({ show, setShow, title, description, handleDeleteCard, listTi
                     <Typography>リスト: {listTitle}</Typography>
                     <Divider />
                     <TextField className={classes.textArea} fullWidth multiline value={newTitle} onChange={handleOnChange} />
-                    <Typography className={classes.explainTitle}>説明 : </Typography>
-                    <TextField className={classes.explainText} fullWidth multiline value={newDes} onChange={handleOnChangeDes} placeholder="詳しい説明を追加してください" />
+
+                    <Typography className={classes.explainTitle}>
+                        説明&nbsp;&nbsp;
+                        <span className={open ? classes.active : classes.btn} onClick={() => setOpen(true)}>編集</span>
+                        &nbsp;&nbsp;
+                        <span className={!open ? classes.active : classes.btn} onClick={() => setOpen(false)}>プレビュー</span>
+                    </Typography>
+                    {open ? (
+                        <div>
+                            <TextField
+                                fullWidth
+                                multiline
+                                autoFocus
+                                onFocus={onFocus}
+                                className={classes.explainText}
+                                onChange={handleOnChangeDes}
+                                value={newDes}
+                                placeholder="詳しい説明を追加してください" />
+                            <span className={classes.subtitle}>マークダウン入力可能</span>
+                        </div>
+                    ) : (
+                            <div>
+                                <ReactMarkdown
+                                    source={newDes}
+                                    renderers={{
+                                        code: Component
+                                    }} />
+                            </div>
+                        )}
+
                 </DialogContent>
                 <DialogActions>
                     <Button color="default" onClick={handleClose}>キャンセル</Button>
@@ -83,6 +140,14 @@ const DialogBox = ({ show, setShow, title, description, handleDeleteCard, listTi
         </div>
     )
 }
+
+const Component = ({ value, language }) => {
+    return (
+        <SyntaxHighlighter language={language ?? null} style={docco}>
+            {value ?? ''}
+        </SyntaxHighlighter>
+    );
+};
 
 const Card = ({ card, index, listId, listTitle }) => {
     const classes = useStyle();
@@ -111,8 +176,10 @@ const Card = ({ card, index, listId, listTitle }) => {
                             {card.title}
                             {card.description.length !== 0 ?
                                 <div>
-                                |&nbsp;
-                                <span className={classes.subtitle}>説明あり</span>
+                                    <DescriptionIcon style={{
+                                        fontSize: '1.2rem',
+                                        color: '#959595',
+                                    }} />
                                 </div>
                                 : ''}
                         </Paper>
